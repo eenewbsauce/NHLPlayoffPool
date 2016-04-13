094be5dca5a14ef4e0868dc4e6adc3a536e324ac
Tracey, Ryan <RyanTracey@rockventures.com>
2016-04-13 16:16:10 -0400

    get player aggregages established

--- app/index.js
+++ app/index.js
@@ -1,1 +1,38 @@
-console.log('hello world')
+'use strict';
+let R = require('ramda');
+let teams = require('../data/teams.json').data;
+let teamsPlayoffs = require('../data/teams_playoffs.json').data;
+
+let players = require('../data/players.json').data;
+let playersPlayoffs2013 = require('../data/players_playoffs_2013.json').data;
+let playersPlayoffs2014 = require('../data/players_playoffs_2014.json').data;
+let playersPlayoffs2015 = require('../data/players_playoffs_2015.json').data;
+
+let playerData = [];
+
+class AggregatePlayer {
+	constructor() {
+		this.regular = {};
+		this.playoff = {};		
+	}
+}
+
+(function main() {
+	console.log(playerData);
+	aggregatePlayerData();
+	console.log(playerData);
+})()
+
+function aggregatePlayerData() {
+	players.forEach(player => {
+		let aggPlayer = new AggregatePlayer();    
+		aggPlayer.regular[2016] = player;
+		aggPlayer.playoff[2015] = R.find(R.propEq('playerId', player.playerId))(playersPlayoffs2015);
+		aggPlayer.playoff[2014] = R.find(R.propEq('playerId', player.playerId))(playersPlayoffs2014);
+		aggPlayer.playoff[2013] = R.find(R.propEq('playerId', player.playerId))(playersPlayoffs2013);
+    
+    playerData.push(aggPlayer);       		
+	});    
+}
+
+
